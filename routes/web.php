d00133e86999fa9c8139bddb5e3be3ed794d9c88
mousaayesh <mousaayesh04@gmail.com>
2024-12-22 22:09:15 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\AddressController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('finest');
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
